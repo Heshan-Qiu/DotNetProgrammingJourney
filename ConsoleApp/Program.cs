9347669db2a9fa623b615a676251419c59aaffa0
Heshan <heshan.chiu@gmail.com>
2023-06-15 20:54:32 +1200

    Daily commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp;
 
-SQLiteCharIssue.StoreCharInSQLite();
-SQLiteCharIssue.UseGetCharExample();
 SQLiteCharIssue.HowToStoreAndGetCharInSQLite();
